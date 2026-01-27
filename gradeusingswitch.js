e91086cd038ad04be17128d2a9b8e1198b6df9df
Ramadevi-89 <122200073+Ramadevi-89@users.noreply.github.com>
2026-01-27 16:50:58 +0530

    Add files via upload
    week 1 assignments

---
+++ gradeusingswitch.js
@@ -0,0 +1,38 @@
+    //grade marks
+
+    let score =90
+
+        switch (true){
+    case (score>=90):
+    console.log("Grade A")
+    break;
+
+    case(score<90 && score>=80):
+console.log("Grade B")
+break;
+
+case (score<80 && score>=70):
+console.log("Grade c")
+break;
+
+case (score<70 && score>=60):
+console.log("Grade D")
+break;
+
+case (score<60 && score>=50):
+console.log("Grade E")
+break;
+
+default:
+console.log("Fail")
+
+
+
+        }
+        
+
+
+
+
+
+
