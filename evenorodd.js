50ccaadf2156b5c9939bbf5d66e99477407be303
Ramadevi-89 <122200073+Ramadevi-89@users.noreply.github.com>
2026-01-27 16:41:34 +0530

    Add files via upload
    week 1 assingments

---
+++ evenorodd.js
@@ -0,0 +1,11 @@
+// print odd/even numbers 
+
+for(let i=1; i<=10; i++){
+    if(i%2!=0){
+console.log(i + " is a Odd number")
+    }
+    else{
+        console.log(i +" is a Even number")
+    }
+
+}
---
+++ numbertype.js
@@ -0,0 +1,14 @@
+//number type
+
+    let num = -25
+    if( num===-25){
+    console.log("the given number is negative number")
+        
+    }
+    else if(num===25){
+        console.log("The given number is positive")
+    }
+    else{
+
+        console.log("number is zero")
+    }
