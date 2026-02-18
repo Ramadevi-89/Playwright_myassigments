62f487168f4376b126eb28be4d37db4a7c42ab76
Ramadevi-89 <122200073+Ramadevi-89@users.noreply.github.com>
2026-02-18 17:32:56 +0530

    Add files via upload
    File Upload- home Assignments

---
+++ Fileupload-HA.spec.ts
@@ -0,0 +1,23 @@
+import test from '@playwright/test'
+
+test("Upload file heroku", async({page})=>{
+await page.goto("https://the-internet.herokuapp.com/upload")
+
+await page.locator("//input[@type='file']").first().setInputFiles('Data/Create_Lead.pdf')
+await page.waitForTimeout(7000)
+//await page.locator("//div[@id='drag-drop-upload']").setInputFiles('Data/da-neuro.png')
+//await page.waitForTimeout(5000)
+//await page.locator("//input[@id='file-submit']").click()
+
+    const fileupload=page.waitForEvent('filechooser') //create promise
+    await page.locator("#drag-drop-upload").first().click()//action
+    const filechooser=await fileupload //file chooser
+    await filechooser.setFiles("Data/da-neuro.png") //resolve the promise
+    await page.locator("//input[@id='file-submit']").click()
+
+
+
+
+
+
+})
